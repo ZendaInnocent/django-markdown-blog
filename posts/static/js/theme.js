b9a3802ddd247cc64fd400544718966d3964ecd6
Innocent Zenda <zendainnocent@gmail.com>
2021-06-21 10:32:48 +0300

    fix: theme fallback to user system mode

--- posts/static/js/theme.js
+++ posts/static/js/theme.js
@@ -1,21 +1,21 @@
 const themeBtn = document.querySelector('.theme-toggler');
 const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
-
-if (prefersDarkTheme.matches) {
-  localStorage.setItem('theme', 'dark');
-}
-
 const currentTheme = localStorage.getItem('theme');
 
 if (currentTheme == 'dark') {
   // set dark theme
   document.body.setAttribute('data-theme', 'dark');
   themeBtn.innerHTML = 'Light Mode';
-} else {
+} else if (currentTheme == 'light') {
   // set light theme
   document.body.setAttribute('data-theme', 'light');
   themeBtn.innerHTML = 'Dark Mode';
+} else {
+  if (prefersDarkTheme.matches) {
+    document.body.setAttribute('data-theme', 'dark');
+  }
 }
+
 
 themeBtn.addEventListener('click', function () {
   const theme = document.body.getAttribute('data-theme');

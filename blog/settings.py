2fb9b79757a0905b429d10e8ff32885df05269b9
Innocent Zenda <zendainnocent@gmail.com>
2021-06-21 11:13:24 +0300

    admin customization

--- blog/settings.py
+++ blog/settings.py
@@ -127,7 +127,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Dar_es_salaam'
 
 USE_I18N = True
 
--- posts/admin.py
+++ posts/admin.py
@@ -2,10 +2,19 @@ from django.contrib import admin
 
 from posts.models import Post, Tag
 
+admin.site.site_title = 'Elegant Tech Admin'
+admin.site.site_header = 'Elegant Tech Administration'
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title', )}
+    list_display = ['title', 'created_on', 'updated_on', ]
+    search_fields = ['title', 'excerpt', 'content', 'tags__name', ]
+    list_filter = ['created_on', 'updated_on', ]
+    list_select_related = True
+    save_on_top = True
+    filter_horizontal = ['tags', ]
 
 
 @admin.register(Tag)

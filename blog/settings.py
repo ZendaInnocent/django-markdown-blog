da4b9199b848db0573e68fefb3307fbba8aa8fc6
Innocent Zenda <zendainnocent@gmail.com>
2021-06-21 11:18:15 +0300

    update database settings

--- blog/settings.py
+++ blog/settings.py
@@ -91,8 +91,11 @@ if not DEBUG:
             'NAME': config('DB_NAME'),
             'USER': config('DB_USER'),
             'PASSWORD': config('DB_PASSWORD'),
-            'HOST': config('DB_HOST'),
-            'PORT': config('DB_PORT', cast=int)
+            'HOST': config('DB_HOST', default='127.0.0.1'),
+            'PORT': config('DB_PORT', cast=int, default=3306),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            },
         }
     }
 else:
